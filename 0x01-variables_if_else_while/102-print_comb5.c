ea65471779fc29351664897acc13cb8fe036376b
Ostentive <104060490+Ostentive@users.noreply.github.com>
2022-09-13 20:55:05 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#include <stdlib.h> #include <time.h>
-
-main - Entry point
-
-Return: Always 0
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
 
 int main(void)
+{
+    int p, q;
 
-int p. of
+    for (p = 0; p <= 98; p++)
+    {
+        for (q = p + 1; q <= 99; q++)
+        {
+            putchar((p / 10) + '0');
+            putchar((p % 10) + '0');
+            putchar(' ');
+            putchar((q / 10) + '0');
+            putchar((q % 10) + '0');
 
-for (p = 0; p <= 98; p++)
+            if (p == 98 && q == 99)
+                continue;
 
-( for (q=p + 1; q<= 99; q++) (
+            putchar(',');
+            putchar(' ');
+        }
+    }
 
-patchart(p/10) + '0'); patchar((p )+'0'):
+    putchar('\n');
 
-patchart''
-
-patchar((q/10) + '0') patchart(q)+ '0');
-
-if (p == 98 88 q = 99)
-
-continue;
-
-patchar('.");
-
-putchar(' ')
-
-patchar('\n');
-return (0);
+    return (0);
 }
