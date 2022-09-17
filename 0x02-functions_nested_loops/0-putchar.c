87e6507cda3880ac87ee63e1efc1778172c91f32
Ostentive <104060490+Ostentive@users.noreply.github.com>
2022-09-17 04:14:41 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 /*
- * File: 0-holberton.c
- * Auth: Brennan D Baraban
- *
- */
+* File:	0-holberton.c
+* Auth:	Brennan	D	Baraban
+*
+*/
 
-#include "main.h"
+#include	"main.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
+* main	-	Prints	"Holberton"	followed	by	a	new	line.
+*
+*	Return:	Always	0.
+*/
 int main(void)
 {
 	_putchar('_');
